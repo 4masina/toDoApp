220ce03c304a7f4c835898eb87a1f15c6aea3a84
4masina <montmom143@gmail.com>
2024-09-19 22:13:47 -0700

    Updating

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return(
+        <footer>
+            <p>© 2022 My Website. All rights reserved.</p>
+            <nav>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <div>
+                <a href="#"><img src="https://www.iconfinder.com/data/icons/logos-and-brands/512/twitter-logo-512.png" alt="Twitter" /></a>
+                <a href="#"><img src="https://www.iconfinder.com/data/icons/logos-and-brands/512/facebook-logo-512.png" alt="Facebook" /></a>
+              </div>
+        </footer>
+    )
+}
+    
+export default Footer;
