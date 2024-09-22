477d2a68a315e14094c24beeaf4476c60c8828be
4masina <montmom143@gmail.com>
2024-09-21 18:51:59 -0700

    Updating

--- src/CheckList.jsx
+++ src/CheckList.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
-const CheckListItem = ({ id, label, text, checked, handleUserClick }) => {
+const CheckListItem = ({ id, label, text, completed, handleUserClick, handleDeleteItem }) => {
     return (
         <div key={id}>
             <label htmlFor={label}>{text}</label>
-            <input onChange = {e => handleUserClick(e)}
-            type="checkbox" name={label} checked={checked} />
-           
+            <input onChange = {(e) => handleUserClick(e)}
+            type="checkbox" name={label} checked={completed} />
+            <button onClick={() => handleDeleteItem(id)}>Delete</button>
+            
         </div>
     );
-};
 
+
+ const checkListComponents = checkListComponents.map((item) => (
+    <CheckListItem
+        key={item.id}
+        id={item.id}
+        label={item.label}
+        text={item.text}
+        completed={item.completed}
+        handleUserClick={handleUserClick}
+        handleDeleteItem={handleDeleteItem}
+    />
+ ));
+ return (
+    <main className='main-container'>
+        {checkListComponents}
+        <input type='text' placeholder='Add new item...' onChange={handleAddItemChange} value={newItemText} />
+        <button onClick={handleAddItem}>Add</button>
+    </main>
+)};
 export default CheckListItem;
